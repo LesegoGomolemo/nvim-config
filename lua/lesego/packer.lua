05dfa3757ba21bd2ebaff4b65200fd76e09ace47
Lesego Mabe <gomolemomabe@gmail.com>
2025-10-04 08:48:46 +0200

    Spacing

--- lua/lesego/packer.lua
+++ lua/lesego/packer.lua
@@ -89,8 +89,8 @@ return require('packer').startup(function(use)
         open_mapping = [[vt]]
       })
     end}
-    
+
     use { "github/copilot.vim" }
-    
+
     -- use('nvim-tree/nvim-tree.lua') 
 end)
