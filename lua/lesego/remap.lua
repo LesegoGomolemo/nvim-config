032371bb05be150e02dd7b49085eb986e28732b8
Lesego Mabe <gomolemomabe@gmail.com>
2026-03-29 15:42:01 +0200

    Fix remaps

--- lua/lesego/remap.lua
+++ lua/lesego/remap.lua
@@ -3,7 +3,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- for some reason these don't work
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
@@ -17,7 +17,7 @@ vim.keymap.set("x", "<leader>p", "\"_dp")
 -- copy into the system clipboard with leader y
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>Y", "\"+Y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
 
 vim.keymap.set("n", "Q", "<nop>")
 
