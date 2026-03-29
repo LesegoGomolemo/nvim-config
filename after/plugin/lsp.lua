ac0d93af443d6b528158f034d24d5508b437053b
Lesego Mabe <gomolemomabe@gmail.com>
2026-03-29 15:34:02 +0200

    Cleanup lsp config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,34 +4,26 @@ lsp.preset('recommended')
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = {'ts_ls', 'rust_analyzer', 'eslint', 'lua_ls', 'html', 'cssls', 'pyright', 'pylsp', 'gopls', 'angularls', 'kotlin_language_server' },
-    handlers = {
-     --   lsp_zero.default_setup,
-    },
+  ensure_installed = {
+    'ts_ls',
+    'rust_analyzer',
+    'eslint',
+    'lua_ls',
+    'html',
+    'cssls',
+    'pyright',
+    'pylsp',
+    'gopls',
+    'angularls',
+    'kotlin_language_server',
+  },
+  handlers = {
+    lsp.default_setup,
+  },
 })
 
-local lspconfig = require('lspconfig')
-lspconfig.ts_ls.setup {}
-lspconfig.lua_ls.setup {
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { 'vim' }
-      }
-    }
-  }
-}
-lspconfig.eslint.setup {}
-lspconfig.html.setup {}
-lspconfig.cssls.setup {}
-lspconfig.pyright.setup {}
-lspconfig.pylsp.setup {}
-lspconfig.gopls.setup {}
-lspconfig.angularls.setup {}
-lspconfig.kotlin_language_server.setup {}
-
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
@@ -46,7 +38,7 @@ lsp.set_preferences({
 
 
 lsp.on_attach(function(client, bufnr)
-    local opts = {buffer = bufnr, remap = false}
+    local opts = { buffer = bufnr, remap = false }
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -61,7 +53,6 @@ lsp.on_attach(function(client, bufnr)
 end)
 
 lsp.setup()
-
 
 cmp.setup({
   mapping = cmp_mappings
