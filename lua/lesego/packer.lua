4ec38e0a8faf960602c98f8b0a7b5fc673d0e86e
Lesego Mabe <lesego@synthesis.co.za>
2026-04-02 09:28:20 +0200

    Mac config

--- lua/lesego/packer.lua
+++ lua/lesego/packer.lua
@@ -77,6 +77,15 @@ return require('packer').startup(function(use)
     end}
     
     use { "github/copilot.vim" }
-    
+
+    -- use {
+    --     "folke/which-key.nvim",
+    --     config = function()
+    --         vim.o.timeout = true
+    --         vim.o.timeoutlen = 200
+    --         require("which-key").setup {
+    --         }
+    --     end
+    -- }
     -- use('nvim-tree/nvim-tree.lua') 
 end)
--- lua/lesego/set.lua
+++ lua/lesego/set.lua
@@ -28,3 +28,6 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.g.mapleader = " "
+
+--vim.opt.spelllang = "en_gb"
+--vim.opt.spell = true
